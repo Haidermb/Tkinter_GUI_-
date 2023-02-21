e4a43eef85b802ce4543a24b6ae0060c0a2f67b6
haidermb <haidermb52@gmail.com>
2023-02-21 13:12:58 +0530

    new tab5 added for all graphs also button added to open a window terminal

--- test16.py
+++ test16.py
@@ -4,13 +4,44 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pandas as pd
 
+
+
+def open_terminal():
+    # Create a new window for the terminal
+    terminal_window = tk.Toplevel(root)
+    terminal_window.title("Terminal")
+    
+    # Create a text widget for the terminal
+    terminal_text = tk.Text(terminal_window)
+    terminal_text.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    
+    # Add some sample text to the terminal
+    terminal_text.insert(tk.END, "Welcome to the terminal!\n")
+    terminal_text.insert(tk.END, "Type your commands here.\n")
+
+    # Add a horizontal scrollbar to the terminal
+    terminal_scrollbar = ttk.Scrollbar(terminal_window, orient="horizontal", command=terminal_text.xview)
+    terminal_text.configure(xscrollcommand=terminal_scrollbar.set)
+    terminal_scrollbar.pack(side="bottom", fill="x")
+
+
+
 # Create a new Tkinter window
 root = tk.Tk()
 root.title("Graphs, Table, and Terminal")
 
+
 # Create a notebook widget to hold multiple tabs
 notebook = ttk.Notebook(root)
 notebook.pack(fill='both', expand=True)
+
+tab5 = ttk.Frame(notebook)
+notebook.add(tab5, text="Graphs (All)")
+
+
+# Create a button to open the terminal window
+terminal_button = ttk.Button(root, text="Open Terminal", command=open_terminal)
+terminal_button.pack(side=tk.RIGHT, padx=1, pady=1 )
 
 # Create a frame for the first tab
 tab1 = ttk.Frame(notebook)
@@ -68,6 +99,36 @@ canvas4 = FigureCanvasTkAgg(fig4, master=tab4)
 canvas4.draw()
 canvas4.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
+
+fig5 = plt.Figure(figsize=(5, 4), dpi=100, tight_layout=True)
+ax1 = fig5.add_subplot(2, 2, 1)
+ax2 = fig5.add_subplot(2, 2, 2)
+ax3 = fig5.add_subplot(2, 2, 3)
+ax4 = fig5.add_subplot(2, 2, 4)
+ax1.plot([1, 2, 3, 4], [10, 20, 30, 40])
+ax2.plot([1, 2, 3, 4], [40, 30, 20, 10])
+ax3.plot([1, 2, 3, 4], [20, 10, 30, 40])
+ax4.plot([1, 2, 3, 4], [30, 40, 10, 20])
+canvas1 = FigureCanvasTkAgg(fig5, master=tab5)
+canvas1.draw()
+canvas1.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+
+
+
+# terminal_frame = ttk.Frame(root)
+# terminal_frame.pack(fill='both', expand=True)
+
+# terminal_text = tk.Text(terminal_frame)
+# terminal_text.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+# terminal_text.insert(tk.END, "Welcome to the terminal!\n")
+# terminal_text.insert(tk.END, "Type your commands here.\n")
+
+# terminal_scrollbar = ttk.Scrollbar(terminal_frame, orient="horizontal", command=terminal_text.xview)
+# terminal_text.configure(xscrollcommand=terminal_scrollbar.set)
+# terminal_scrollbar.pack(side="bottom", fill="x")
+
 # Create a frame for the table
 table_frame = ttk.Frame(root)
 table_frame.pack(fill='both', expand=True)
@@ -75,7 +136,7 @@ table_frame.pack(fill='both', expand=True)
 # Create a Pandas DataFrame to use as the table data
 data = {'Name': ['John', 'Mary', 'Steve', 'Sarah','John', 'Mary', 'Steve', 'Sarah','John', 'Mary', 'Steve', 'Sarah'],
         'Age': [25, 30,50,60,25, 30,50,60,25, 30,50,60],
-         'Email':['t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com','t@gmail.com'],
+         'Email':['test1@gmail.com','test3@gmail.com','test4@gmail.com','test5@gmail.com','5t@gmail.com','t65@gmail.com','45t@gmail.com','t23@gmail.com','32t@gmail.com','213t@gmail.com','t@gmail.com','t@gmail.com'],
          'phone':[12334324,21889787,82738978,12736782,12334324,21889787,82738978,12736782,12334324,21889787,82738978,12736782]   }
 
 df = pd.DataFrame(data)
@@ -92,17 +153,5 @@ table.configure(yscrollcommand=scrollbar.set)
 scrollbar.pack(side="right", fill="y")
 table.pack(side="left", fill="both", expand=True)
 
-terminal_frame = ttk.Frame(root)
-terminal_frame.pack(fill='both', expand=True)
-
-terminal_text = tk.Text(terminal_frame)
-terminal_text.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-terminal_text.insert(tk.END, "Welcome to the terminal!\n")
-terminal_text.insert(tk.END, "Type your commands here.\n")
-
-terminal_scrollbar = ttk.Scrollbar(terminal_frame, orient="horizontal", command=terminal_text.xview)
-terminal_text.configure(xscrollcommand=terminal_scrollbar.set)
-terminal_scrollbar.pack(side="bottom", fill="x")
 
 root.mainloop()
